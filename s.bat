484c28ca8b7dac9c0b34c3462e0acec24a34f493
Vladimir <plotnikvk@gmail.com>
2019-05-16 23:42:10 +0300

    Debug_4

--- s.bat
+++ s.bat
@@ -1,4 +1,4 @@
 del /S /F /Q C:\Users\Administrator\Desktop\project\watir-ruby-cucumber\screenshots\ 
-REM call cucumber --tag @w1 --color
-REM call cucumber --tag @w2 --color
+call cucumber --tag @w1 --color
+call cucumber --tag @w2 --color
 call cucumber --tag @w3 --color
