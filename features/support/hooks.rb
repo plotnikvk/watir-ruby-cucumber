c4e7255dceb2c30a8bbb22ef862d038871837ba4
Vladimir <plotnikvk@gmail.com>
2019-05-16 22:32:02 +0300

    Debug

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,5 @@
 Before do |_scenario|
 begin
-  ENV['REMOTE_HUB'] = 'http://192.168.0.235:4444/wd/hub'
   ####################################
   @url_hub = 'http://192.168.0.235:4444/wd/hub'
   if ENV['REMOTE_HUB']
@@ -13,22 +12,16 @@ begin
   if @local
     @browser = Watir::Browser.new :chrome
   else
-    p 'fgf1'
     capabilities = Selenium::WebDriver::Remote::Capabilities.new
-    p 'fgf2'
     capabilities.browser_name = :chrome
-    p 'fgf3'
     @browser = Watir::Browser.new(
       :remote,
       url: @url_hub,
       desired_capabilities: capabilities
     )
-    p 'fgf'
     @browser.driver.file_detector = lambda do |args|
       str = args.first.to_s
-      p str
       str if File.exist?(str)
-      p str
     end
   end
   @browser.window.maximize
